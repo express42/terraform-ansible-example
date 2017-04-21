7ee6a80732133529d0ded5ecc7d43b32da7a46d7
Andrey Mukhin <snurlux@gmail.com>
2017-04-21 19:15:30 +0300

    Upd terraform_inventory script

--- ansible/terraform_inventory.sh
+++ ansible/terraform_inventory.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 set -euo nounset -o pipefail
+# cd to script dir
+cd $( dirname "${BASH_SOURCE[0]}" )
+# set inventory dir
 INVENTORY_DIR="$( cd ../terraform/.terraform && pwd )"
 ati "$@" --root $INVENTORY_DIR
